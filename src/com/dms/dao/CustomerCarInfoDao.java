79cc3e8402b62d6d825abf7a264166fb1f93b459
ta0 <Administrator@MS-20170801KYAU>
2017-08-24 10:35:39 +0800

    CarInfo

--- src/com/dms/dao/CustomerCarInfoDao.java
+++ src/com/dms/dao/CustomerCarInfoDao.java
@@ -6,7 +6,7 @@ import com.dms.entity.CustomerCarInfo;
 public interface CustomerCarInfoDao {
 	//客户车辆信息操作
 	CustomerCarInfo getCustomerCarInfoByKey(String key); //根据车牌或者车架号，查询获取客户车辆信息
-	//void addCustomerCarInfo(CustomerCarInfo info);  //添加客户车辆信息
-	//void removeCustomerCarInfo(int customerCarInfoId);//根据id删除信息
-	//void updateCustomerCarInfo(CustomerCarInfo info);//修改客户车辆信息				
+	void addCustomerCarInfo(CustomerCarInfo info);  //添加客户车辆信息
+	void removeCustomerCarInfo(int customerCarInfoId);//根据id删除信息
+	void updateCustomerCarInfo(CustomerCarInfo info);//修改客户车辆信息				
 }
