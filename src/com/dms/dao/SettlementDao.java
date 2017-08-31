1013eb12ebe93260395b7f0350b602d1020213ea
Administrator <Administrator@MS-20170801KYAU>
2017-08-31 09:24:37 +0800

    carinfo jsp 修改

--- src/com/dms/dao/SettlementDao.java
+++ src/com/dms/dao/SettlementDao.java
@@ -1,7 +1,15 @@
 package com.dms.dao;
 
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.dms.entity.CustomerCarInfo;
 import com.dms.entity.Order;
 
 public interface SettlementDao {
-		public Order getSettlementByWordKey();
+		public List<Order> getSettlementByWordKey(@Param("completedDate") Date completedDate,@Param("SA") Integer SA,
+				@Param("orderStateId") Integer orderStateId, @Param("customerCarInfo") CustomerCarInfo customerCarInfo,
+				@Param("orderId") Integer orderId);
 }
