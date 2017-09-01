91f63c31b87fb5f5eab0b14c2a1864ea2a57e3be
t <Administrator@MS-20170801KYAU>
2017-09-01 14:20:19 +0800

    结算

--- src/com/dms/service/impl/SettlementServiceImpl.java
+++ src/com/dms/service/impl/SettlementServiceImpl.java
@@ -1,29 +1,35 @@
 package com.dms.service.impl;
 
-import java.util.Date;
 import java.util.List;
 
-import com.dms.dao.SettlementDao;
+import com.dms.dao.OrderDao;
 import com.dms.entity.CustomerCarInfo;
 import com.dms.entity.Order;
 import com.dms.service.SettlementService;
 
-public class SettlementServiceImpl implements  SettlementService{
+public class SettlementServiceImpl implements SettlementService {
 	
-	private SettlementDao settlementdao ;
-	
-	public SettlementDao getSettlementDao(){
-		return settlementdao ;
+	private OrderDao orderDao ;
+		
+	public OrderDao getOrderDao() {
+		return orderDao;
 	}
-	public void setSettlementDao(SettlementDao settlementdao){
-		this.settlementdao = settlementdao;
+
+	public void setOrderDao(OrderDao orderDao) {
+		this.orderDao = orderDao;
 	}
-	
+
 	@Override
-	public List<Order> getSettlementByWordKey(Date completedDate, Integer SA, Integer orderStateId,
+	public List<Order> getSettlementByWordKey(String completedDate, String SA, Integer orderStateId,
 			CustomerCarInfo customerCarInfo, Integer orderId) {
-		List<Order> order = settlementdao.getSettlementByWordKey(completedDate, SA, orderStateId, customerCarInfo, orderId);
-		return order;
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getSettlementAll() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
